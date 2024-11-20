7813d494623ebbfab52a53ef9ae9a10d513285a9
Kamal KHATIM <kamalkhatim@MacBook-Pro-de-Kamal-2.local>
2024-11-20 03:26:02 +0100

    ouvrature menu

---
+++ app.js
@@ -0,0 +1,9 @@
+const btnMenu = document.querySelector('.btn-rond-menu');
+const nav = document.querySelector('.nav-gauche');
+const allItemNav = document.querySelectorAll('.nav-menu-item');
+const ligne = document.querySelector('.cont-ligne');
+
+btnMenu.addEventListener('click', () => { 
+    ligne.classList.toggle('active')
+    nav.classList.toggle('menu-visible')
+})
